cfd59f6cd41237afd1fb39238d94fdce9a91da04
Chandanbm45 <chandanbm44@gmail.com>
2025-10-30 15:28:12 +0530

    Implemented Login, Main and Navbar with Tailwind setup

--- TwinCart/src/App.jsx
+++ TwinCart/src/App.jsx
@@ -1,15 +1,39 @@
-import React from "react";
-import Navbar from "./Components/Navbar/Navbar.jsx";
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Navbar from "./Components/Navbar/Navbar.jsx";
+// import Login from "./Components/LoginPopup/LoginPopup.jsx";
+
+
+// const App = () => {
+//   return (
+//     <>
+//     <Router>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<h1 style={{textAlign:'center'}}>🏬 Welcome to TwinCart</h1>} />
+//         <Route path="/offers" element={<h1 style={{textAlign:'center'}}>🔥 Offers Page</h1>} />
+//         <Route path="/about" element={<h1 style={{textAlign:'center'}}>📘 About Us</h1>} />
+//         <Route path="/contact" element={<h1 style={{textAlign:'center'}}>📞 Contact Us</h1>} />
+//         <Route path="/login" element={<Login />} />
+//       </Routes>
+//     </Router>
+   
+//     </>
+//   );
+// };
+
+// export default App;
+
+import React from 'react'
+import Main from './Components/Main/Main.jsx'
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <h1 style={{ textAlign: "center", marginTop: "100px" }}>
-        🛒 Welcome to Twin Mart — Shopping App
-      </h1>
+      <Main />
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- TwinCart/src/Components/Navbar/Navbar.jsx
+++ TwinCart/src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="nav-logo">
-        <img src="/images/logo.png" alt="TwinMart Logo" />
-        <h2>Twin Mart</h2>
+        <Link to="/">
+          <img src="/logo.png" alt="TwinCart Logo" />
+        </Link>
+        <h2>TwinCart</h2>
       </div>
 
       <ul className="nav-links">
-        <li><a href="/">Home</a></li>
-        
-        <li><a href="/offers">Offers</a></li>
-        <li><a href="/About">About Us</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/offers">Offers</Link></li>
+        <li><Link to="/about">About Us</Link></li>
+        <li><Link to="/contact">Contact Us</Link></li>
       </ul>
 
       <div className="nav-actions">
-        
-        <button className="login-btn">Login</button>
+        <Link to="/login">
+          <button className="login-btn">Login</button>
+        </Link>
       </div>
     </nav>
   );
