035f3b8614beb6530b0afac9b6af815c890575b5
AshishDD19 <ashishdevajan2@gmail.com>
2025-10-30 15:57:33 +0530

    Testimonials added

--- TwinCart/src/App.jsx
+++ TwinCart/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
 import Footer from './components/Footer/Footer'
+import Testimonials from './components/Testimonials/Testimonials'
 
 function App() {
 
   return (
     <>
+    <Testimonials/>
     <Footer/>
     </>
   )
